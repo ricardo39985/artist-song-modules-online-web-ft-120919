ca17019482dfa9e6a2d13ec775e67b90449f03a7
Ricardo Persaud <ricardo39985@gmail.com>
2020-01-07 18:57:25 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -6,10 +6,17 @@ module Memorable
   def count
     self.all.count
   end
+  module InstanceMethods
+    def initialize
+      self.class.all << self
+    end
+  end
 end
 class Song
   extend Memorable
+  include Memorable::InstanceMethods
 end
 class Artist
   extend Memorable
+  include Memorable::InstanceMethods
 end
